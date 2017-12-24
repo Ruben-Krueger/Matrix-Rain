bc2990037b75be3a789c067e187c2e9393753f43
Ruben Krueger <rubenkrueger99@gmail.com>
2017-12-23 23:56:01 -0800

    added files

---
+++ matrixRain.js
@@ -0,0 +1,72 @@
+var maxCharactersInStream = 40;
+var minCharactersInStream = 10;
+var streams = [];
+var maxTextSize = 20;
+var minTextSize = 8;
+var minChineseCharacter = 11904;
+var maxChineseCharacter = 12019;
+var maxStreams = 50;
+var maxSpeed = 20;
+var minSpeed = 4;
+var frameModulo = 50;
+
+function setup() {
+  createCanvas(window.innerWidth, window.innerHeight);
+  createCharacterStreams();
+}
+
+function draw() {
+  clear();
+  background(0);
+  updateCharacterStreams();
+}
+
+function randomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function createCharacterStreams() {
+  for(var i = 0; i < maxStreams; i ++ ){
+    var randomStreamLength = randomNumber(minCharactersInStream, maxCharactersInStream);
+    var randomX = randomNumber(0, window.innerWidth);
+    var randomY = randomNumber(0, window.innerHeight);
+    var randomTextSize = randomNumber(minTextSize, maxTextSize);
+    var randomSpeed = randomNumber(minSpeed, maxSpeed);
+    var stream = new Stream(randomX, randomY, randomStreamLength, randomTextSize, randomSpeed);
+    streams.push(stream);
+  }
+}
+
+function updateCharacterStreams() {
+    for(var i = 0; i < streams.length; i ++ ){
+     if(frameCount % frameModulo === 0) streams[i].draw();
+     streams[i].update();
+  }
+}
+
+
+
+function Stream(x, y, numberOfCharacters, textsize, speed) {
+  this.x = x;
+  this.y = y;
+  
+  this.draw = function() {
+    for(var i = 0; i < numberOfCharacters; i ++) {
+        var randomInt = randomNumber(minChineseCharacter, maxChineseCharacter);
+        var character =  String.fromCharCode(randomInt);
+        textSize(textsize);
+        fill(0, 255, 0);
+        text(character, x, y - i * textsize);
+    }
+  }
+  
+  this.update = function() {
+    if(y - numberOfCharacters * textsize > window.innerHeight) {
+      y = 0;
+    }
+    else {
+      y += speed;
+  }
+  }
+}
+
